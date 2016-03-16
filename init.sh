0784680973ccf9bf07b04cdbaae16f307d6dac88
Sergey Sklyar <svitch@gmail.com>
2016-03-17 00:49:46 +0300

    upd

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/hello.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application

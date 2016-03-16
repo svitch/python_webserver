f07aa181fd3adda1ca0e58e543063ac2ff6b1606
Sergey Sklyar <svitch@gmail.com>
2016-03-17 02:21:45 +0300

    kurotkin

--- init.sh
+++ init.sh
@@ -1,4 +1,8 @@
-#!/usr/bin/env bash
-sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -s /home/sergey/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx start
+ps -o pid,euser,egroup,comm,args -C nginx
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+
+
+

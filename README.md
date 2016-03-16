6ec3aafdfcee3cc8cf21f18e846609d5cd78fc32
Sergey Sklyar <svitch@gmail.com>
2016-03-17 01:42:01 +0300

    first

--- README.md
+++
@@ -1,7 +0,0 @@
-# stepic_web_project
-For start -> 
-
-	sudo /etc/init.d/nginx start
-	ps -o pid,euser,egroup,comm,args -C nginx
-	git clone https://github.com/kurotkin/stepic_web_project.git /home/box/web
-	bash /home/box/web/init.sh
--- init.sh
+++ init.sh
@@ -1,6 +1,3 @@
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/hello.conf
+#!/usr/bin/env bash
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/hello
-sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
-sudo /etc/init.d/gunicorn restart

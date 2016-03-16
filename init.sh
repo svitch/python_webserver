26365db88b4b0155f1aaa0f2c34c2fb65c817af3
Sergey Sklyar <svitch@gmail.com>
2016-03-17 02:25:56 +0300

    kurotkin_2nd

--- init.sh
+++ init.sh
@@ -1,8 +1,5 @@
-sudo /etc/init.d/nginx start
-ps -o pid,euser,egroup,comm,args -C nginx
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-
-
-
-
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
+sudo /etc/init.d/gunicorn restart

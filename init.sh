26e2daa2ff0947ab7556cd808f4d25962d5f4a55
Sergey Sklyar <svitch@gmail.com>
2016-03-17 01:00:32 +0300

    upd2

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/hello.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/hello.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/hello.conf
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
 sudo /etc/init.d/gunicorn restart
